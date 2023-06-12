83a0a4e16fb9aadd6644ac3840f2832f2d2f9748
PikPikcU <60111811+pikpikcu@users.noreply.github.com>
2023-06-13 03:19:57 +0700

    added

--- nodecraw.js
+++ nodecraw.js
@@ -1,6 +1,4 @@
-#!/usr/bin/node
-
-import { PlaywrightCrawler, Dataset } from 'crawlee';
+import { PlaywrightCrawler, LogLevel, Log, Dataset, CheerioCrawler } from 'crawlee';
 import puppeteer from 'puppeteer';
 import crawler from 'crawler';
 import axios from 'axios';
@@ -9,18 +7,53 @@ import chalk from 'chalk';
 import fs from 'fs';
 import { URL } from 'url';
 
+const log = new Log({ level: LogLevel.OFF });
+
 commander
   .option('-u, --url <url>', 'Specify a single target URL')
   .option('-l, --list <file>', 'Specify a list of target URLs')
+  .option('-a, --aggressive <maxConcurrency>', 'Set the maximum concurrency for aggressive crawling', parseInt)
+  .option('-r, --recursive', 'Enable recursive crawling')
   .option('-t, --timeout <timeout>', 'Specify the timeout duration')
   .option('-o, --output <file>', 'Specify the output file')
   .parse(process.argv);
 
-const { url, list, output, timeout } = commander.opts();
+const { url, list, output, timeout, recursive, aggressive } = commander.opts();
+const maxConcurrency = aggressive || 5;
 
 if (!url && !list) {
   console.error(chalk.red('Error: Please specify either a URL or a target list.'));
   commander.help();
+}
+
+async function sortAndWriteOutput(data) {
+  if (!output) {
+    return;
+  }
+
+  let formattedData;
+
+  try {
+    formattedData = JSON.stringify([...new Set(data)], null, 2);
+  } catch (error) {
+    console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+    return;
+  }
+
+  const fileExtension = output.slice(output.lastIndexOf('.') + 1);
+
+  try {
+    if (fileExtension === 'txt') {
+      const sortedData = [...new Set(data)].sort();
+      fs.writeFileSync(output, sortedData.join('\n'));
+    } else if (fileExtension === 'json') {
+      fs.writeFileSync(output, formattedData);
+    } else {
+      console.error(chalk.red('Error: Invalid output file extension.'));
+    }
+  } catch (error) {
+    console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+  }
 }
 
 async function crawlUrl(targetUrl) {
@@ -32,8 +65,26 @@ async function crawlUrl(targetUrl) {
       if (output) {
         fs.appendFileSync(output, `${request.loadedUrl}\n`);
       }
+      if (recursive) {
+        await enqueueLinks();
+      }
+    },
+    log: log,
+    maxConcurrency: maxConcurrency,
+  });
+
+  // CheerioCrawler
+  const cheerioCrawler = new CheerioCrawler({
+    async requestHandler({ request, response, $, enqueueLinks }) {
+      console.log(request.uri.href);
+      await Dataset.pushData({ url: request.uri.href });
+      if (output) {
+        fs.appendFileSync(output, `${request.uri.href}\n`);
+      }
       await enqueueLinks();
     },
+    log: log,
+    maxConcurrency: maxConcurrency,
   });
 
   // Puppeteer
@@ -76,7 +127,7 @@ async function crawlUrl(targetUrl) {
               }
             }
           } catch (error) {
-            // console.error('Error:', error);
+            console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
           }
         }
       };
@@ -85,7 +136,7 @@ async function crawlUrl(targetUrl) {
 
       await browser.close();
     } catch (error) {
-      //console.error('Error:', error);
+      console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
     }
   };
 
@@ -96,7 +147,7 @@ async function crawlUrl(targetUrl) {
       callback: function (error, res, done) {
         if (error) {
           console.error(chalk.red(`Crawler Error for ${targetUrl}:`));
-          console.error(error);
+          console.error(error.response ? error.response.statusText : error.message);
         } else {
           const $ = res.$;
           console.log(chalk.white(`${res.request.uri.href}`));
@@ -125,7 +176,7 @@ async function crawlUrl(targetUrl) {
         }
       }
     } catch (error) {
-      //console.error('Error:', error);
+      console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
     }
   };
 
@@ -138,26 +189,48 @@ async function crawlUrl(targetUrl) {
 
   if (targetUrl) {
     if (targetUrl.startsWith('http')) {
-      await Promise.all([
-        playwrightCrawler.run([targetUrl]),
-        puppeteerCrawler(targetUrl),
-        crawlerCrawl(targetUrl),
-        webArchiveCrawler(targetUrl),
-      ]);
+      try {
+        if (recursive) {
+          await Promise.all([
+            playwrightCrawler.run([targetUrl]),
+            puppeteerCrawler(targetUrl),
+            crawlerCrawl(targetUrl),
+            webArchiveCrawler(targetUrl),
+          ]);
+        } else {
+          await Promise.all([
+            cheerioCrawler.run([targetUrl]),
+            puppeteerCrawler(targetUrl),
+            crawlerCrawl(targetUrl),
+            webArchiveCrawler(targetUrl),
+          ]);
+        }
+      } catch (error) {
+        console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+      }
     } else {
-      //console.error(chalk.red('Error: Invalid URL.'));
+      console.error(chalk.red('Error: Invalid URL.'));
       process.exit(1);
     }
   }
 }
 
 async function crawlUrls(urls) {
+  const data = [];
+
   for (const target of urls) {
     if (target.trim() !== '') {
       const targetUrl = target.startsWith('http') ? target : `http://${target}`;
-      await crawlUrl(targetUrl);
+      try {
+        await crawlUrl(targetUrl);
+        data.push(targetUrl);
+      } catch (error) {
+        console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+      }
     }
   }
+
+  await sortAndWriteOutput(data);
 }
 
 async function readUrlsFromStdin() {
@@ -170,19 +243,31 @@ async function readUrlsFromStdin() {
 
 if (url) {
   const targetUrl = url.startsWith('http') ? url : `http://${url}`;
-  await crawlUrl(targetUrl);
+  try {
+    await crawlUrl(targetUrl);
+    await sortAndWriteOutput([targetUrl]);
+  } catch (error) {
+    console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+    process.exit(1);
+  }
 }
 
 if (list) {
-  const urls = fs.readFileSync(list, 'utf-8').split('\n');
-  crawlUrls(urls);
+  try {
+    const urls = fs.readFileSync(list, 'utf-8').split('\n');
+    await crawlUrls(urls);
+  } catch (error) {
+    console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+    process.exit(1);
+  }
 }
 
 if (!process.stdin.isTTY) {
-  readUrlsFromStdin()
-    .then((urls) => crawlUrls(urls))
-    .catch((error) => {
-      console.error(chalk.red('Error:', error));
-      process.exit(1);
-    });
+  try {
+    const urls = await readUrlsFromStdin();
+    await crawlUrls(urls);
+  } catch (error) {
+    console.error(chalk.red('Error:', error.response ? error.response.statusText : error.message));
+    process.exit(1);
+  }
 }
